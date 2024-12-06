503553dbe5e9da9e0ec531e845cfb00197276540
Kyle Den Hartog <kdenhartog@users.noreply.github.com>
2024-12-07 00:12:22 +1300

    fetch the list from github CDN rather than local git submodule

--- scripts/convert-list.js
+++ scripts/convert-list.js
@@ -1,22 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 
-// Load config.json
-let config;
-try {
-  config = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'eth-phishing-detect', 'src', 'config.json'), 'utf8'));
-} catch (error) {
-  console.error(`Error reading config.json: ${error}`);
-  process.exit(1);
+// build filterlist
+async function buildFilterlist(blacklist) {
+  const filterlist = [];
+  for (const domain of Object.values(blacklist)) {
+    // Generate filterlist in UBlock Origin's syntax  
+      filterlist.push('||' + domain + '^$all');
+    }
+  filterlist.sort((a, b) => a.localeCompare(b));
+  
+  // Write filterlist to file
+  await fs.writeFileSync(path.join(__dirname, '..', 'filterlist', 'eth-phishing-list.filter'), filterlist.join('\n'));
+  console.log('Filterlist generated and written to file!');
 }
 
-// Generate filterlist in UBlock Origin's syntax
-const filterlist = [];
-for (const domain of Object.values(config.blacklist)) {
-    filterlist.push('||' + domain + '^$all');
-  }
-filterlist.sort((a, b) => a.localeCompare(b));
+// Load config.json
+fetch("https://raw.githubusercontent.com/MetaMask/eth-phishing-detect/refs/heads/main/src/config.json")
+.then(async (res) => {
+    if (! res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
 
-// Write filterlist to file
-fs.writeFileSync(path.join(__dirname, '..' , 'filterlist', 'eth-phishing-list.filter'), filterlist.join('\n'));
-console.log('Filterlist generated and written to file!');
+  const response = await res.json();
+  const blacklist = response.blacklist;
+  buildFilterlist(blacklist);
+  })
+.catch((error) => {
+  console.error(`Error reading config.json: ${error}`);
+  process.exit(1);
+});
